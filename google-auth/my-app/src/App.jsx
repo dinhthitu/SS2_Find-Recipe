6c7649db7f8483cf48177db8d519f6a8d8641db7
unknown <dinhthitu>
2025-04-17 11:08:00 +0700

    first-cm

---
+++ google-auth/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from "./pages/Home";
+
+const App = () =>{
+  return (
+    <div className = 'w-full h-screen grid place-items-center'>
+      <Home />
+    </div>
+  )
+}
+
+export default App;
+
